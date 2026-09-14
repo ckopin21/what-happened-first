876fd38a278ed433af2c2954937633b3b6b58448
ckopin21 <ckopin21@gmail.com>
2026-09-14 18:34:09 -0500

    Register fresh Timeline question pack

--- packs/timeline/manifest.js
+++ packs/timeline/manifest.js
@@ -2,7 +2,10 @@
 // through this manifest. The default is synchronously loaded for GitHub Pages.
 window.TIMELINE_PACK_MANIFEST={
   defaultPackId:"timeline-general-knowledge-v3",
-  packs:[{id:"timeline-general-knowledge-v3",name:"General Knowledge",genre:"Timeline",file:"current.js"}]
+  packs:[
+    {id:"timeline-general-knowledge-v3",name:"General Knowledge",genre:"Timeline",file:"current.js"},
+    {id:"timeline-general-knowledge-v4",name:"General Knowledge II",genre:"Timeline",file:"timeline-general-knowledge-v4.js"}
+  ]
 };
 window.TIMELINE_QUESTION_PACKS=window.TIMELINE_QUESTION_PACKS||{};
 (function(){const p=window.TIMELINE_PACK_MANIFEST.packs.find(x=>x.id===window.TIMELINE_PACK_MANIFEST.defaultPackId);if(p)document.write('<script src="../packs/timeline/'+p.file+'"><'+'/script>');})();
