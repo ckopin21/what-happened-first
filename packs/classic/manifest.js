916140f09702da8804f8938d477415a843cca9c8
ckopin21 <ckopin21@gmail.com>
2026-09-14 18:33:58 -0500

    Register fresh Classic question pack

--- packs/classic/manifest.js
+++ packs/classic/manifest.js
@@ -2,7 +2,10 @@
 // through this manifest. The default is synchronously loaded for GitHub Pages.
 window.CLASSIC_PACK_MANIFEST={
   defaultPackId:"general-knowledge-v3",
-  packs:[{id:"general-knowledge-v3",name:"General Knowledge",genre:"General Knowledge",file:"current.js"}]
+  packs:[
+    {id:"general-knowledge-v3",name:"General Knowledge",genre:"General Knowledge",file:"current.js"},
+    {id:"general-knowledge-v4",name:"General Knowledge II",genre:"General Knowledge",file:"general-knowledge-v4.js"}
+  ]
 };
 window.CLASSIC_QUESTION_PACKS=window.CLASSIC_QUESTION_PACKS||{};
 (function(){const p=window.CLASSIC_PACK_MANIFEST.packs.find(x=>x.id===window.CLASSIC_PACK_MANIFEST.defaultPackId);if(p)document.write('<script src="../packs/classic/'+p.file+'"><'+'/script>');})();
